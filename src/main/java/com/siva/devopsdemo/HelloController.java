4e35a15b74bc2a987d1bdb18446a41ba40f1a931
Siva <sivasubramanian0791@gmail.com>
2026-06-22 03:42:26 +0530

    Initial Commit

---
+++ src/main/java/com/siva/devopsdemo/HelloController.java
@@ -0,0 +1,13 @@
+package com.siva.devopsdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Siva - Jenkins Docker AWS Project";
+    }
+}
